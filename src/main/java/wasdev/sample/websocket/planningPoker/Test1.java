73f41e60c95a2458e963d5c2bc29ff04f93bcbcd
sword <sword@localhost>
2016-03-28 11:50:08 +0800

    fix java source

--- src/main/java/wasdev/sample/websocket/planningPoker/Test1.java
+++ src/main/java/wasdev/sample/websocket/planningPoker/Test1.java
@@ -6,8 +6,8 @@ import java.io.InputStream;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
-public class Test {
-	public Test() throws IOException {
+public class Test1 {
+	public Test1() throws IOException {
 		String templatePath = "Sample_v0.6.xls";
 		InputStream input = new FileInputStream(templatePath);
 		HSSFWorkbook wb = new HSSFWorkbook(input);
